8718ada57498d6a3b495c5036a05dc84ac587154
litao <litaotju@live.cn>
2015-06-30 21:07:10 +0800

    vm_parser_tested_right
    初步使用b19.v测试通过.

--- netlist_parser.py
+++ netlist_parser.py
@@ -6,17 +6,21 @@ Created on Mon Jun 29 21:13:37 2015
 """
 import lex
 states = (
+   ('timescale','exclusive'),
    ('linecomment','exclusive'),
    ('blockcomment','exclusive'),
-   ('timescale','exclusive'),
+   ('string','exclusive')
 )
 
-literals = ['.',';','(',')','[',':',']','=',"'"]
+literals = [',','.',';','(',')','[',':',']','=',"'",'{','}','"']
 token_raw = [
    'NUMBER',
    'IDENTIFIER',
    'VECTOR',
-   'HEX_NUMBER'
+   'BIT',
+   'HEX_NUMBER',
+   'BIN_NUMBER',
+   'STRING_CON'
    ]
 reserved={
    'module' : 'MODULE',
@@ -25,42 +29,53 @@ reserved={
    'output' : 'OUTPUT',
    'inout' :'INOUT',
    'defparam':'DEFPARAM',
+   'INIT'   :'INIT',
    'assign':'ASSIGN',
-   'endmodule':'ENDMODULE'   
+   'endmodule':'ENDMODULE',   
 }
 tokens=token_raw+list(reserved.values())
 ##########################################################
 #注释与timescale部分,没有返回token
 def t_timescale(t):
-    r'\`'
+    r'(\`timescale.*)'
     t.lexer.begin('timescale')
 #comment section
 def t_linecomment(t):
-    r'\/\/.*'
+    r'//.*'
     t.lexer.begin('linecomment')
-def t_timescale_linecomment_error(t):
-    t.lexer.skip(1)
-    pass
 def t_timescale_linecomment_end(t):
     r'\n'
     t.lexer.lineno += 1
     t.lexer.begin('INITIAL') 
 
-def t_blockcomment(t):
+def t_to_blockcomment(t):
     r'\/\*'
     t.lexer.begin('blockcomment')
-def t_blockcomment_error(t):
-    t.lexer.skip(1)
-    pass
 def t_blockcomment_end(t):
     r'\*\/'
     t.lexer.begin('INITIAL')
+def t_to_string(t):
+    r'"'
+    t.type='"'
+    t.lexer.begin('string')
+    return t
+def t_string_STRING_CON(t):
+    r'[^"]+'
+    return t
+def t_string_end(t):
+    r'"'
+    t.type='"'
+    t.lexer.begin('INITIAL')
+    return t
 ##########################################################
+def t_BIN_NUMBER(t):
+    r'\d+\'b[0-1]+'
+    return t
 def t_HEX_NUMBER(t):
     r'\d+\'h[0-9A-F]+'
     return t
 def t_words(t):
-    '\\\\?[a-zA-Z_]+[\\\\\w\d_\.\[\]]*'
+    '\\\\?[a-zA-Z_]+[\\\\\w\.]*'
     t.type = reserved.get(t.value,'IDENTIFIER')
     return t
 def t_NUMBER(t):
@@ -70,8 +85,19 @@ def t_NUMBER(t):
 def t_VECTOR(t):
     r'\[\d+\:\d+\]'
     return t
+def t_BIT(t):
+    r'\[\d+\]'
+    return t
 ##########################################################
-#标点符号部分    
+#标点符号部分
+def t_flbracket(t):
+    r'\{'
+    t.type='{'
+    return t
+def t_frbracket(t):
+    r'\}'
+    t.type='}'
+    return t    
 def t_lbracket(t):
     r'\('
     t.type='('
@@ -88,10 +114,8 @@ def t_rseqbracket(t):
     r'\]'
     t.type=']'
     return t
-#def t_comma(t):
-#    r'\,'
-#    t.type=','
-#    return t
+def t_comma(t):
+    r'\,'
 def t_period(t):
     r'\.'
     t.type='.'
@@ -112,7 +136,7 @@ def t_singlequote(t):
     r'\''
     t.type="'"
     return t
-def t_newline(t):
+def t_ANY_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)    
 ##########################################################
@@ -120,13 +144,13 @@ def t_newline(t):
 def t_userignore(t):
     r'\s'
     pass
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+def t_ANY_error(t):
+    #print "Illegal character '%s' in line : %d %s " %(t.value[0],t.lexer.lineno,lexer.lexstate)
     t.lexer.skip(1)
 # Build the lexer
 lexer = lex.lex()
 
-
+#####################################################################################################
 import yacc
 def p_vmfile(p):
     '''vm_file : module_decl port_decl_list signal_decl_list primitive_list assign_stm_list ENDMODULE
@@ -142,7 +166,6 @@ def p_pipo_list(p):
                  | IDENTIFIER
     '''
     pass
-
 def p_port_decl(p):
     '''port_decl_list : port_decl_list port_decl
                      | port_decl
@@ -164,35 +187,64 @@ def p_signal_decl_list(p):
 def p_primitive_list(p):
     '''primitive_list : primitive_list primitive
                       | primitive
-       primitive      : IDENTIFIER IDENTIFIER '(' primitive_port_list ')' ';'
-                      | IDENTIFIER IDENTIFIER '(' primitive_port_list ')' ';' defparam
-       defparam       : DEFPARAM IDENTIFIER '.' IDENTIFIER '=' HEX_NUMBER ';'
+       primitive      : IDENTIFIER IDENTIFIER     '(' primitive_port_list ')' ';'
+                      | IDENTIFIER IDENTIFIER BIT '(' primitive_port_list ')' ';' 
+                      | IDENTIFIER IDENTIFIER     '(' primitive_port_list ')' ';'    defparam_list
+                      | IDENTIFIER IDENTIFIER BIT '(' primitive_port_list ')' ';'    defparam_list
+                      | IDENTIFIER IDENTIFIER VECTOR '(' primitive_port_list ')' ';' defparam_list
+       defparam_list  : defparam_list defparam_stm
+                      | defparam_stm
+       defparam_stm   : DEFPARAM IDENTIFIER              '=' HEX_NUMBER ';'
+                      | DEFPARAM IDENTIFIER        '.' INIT '=' HEX_NUMBER ';'
+                      | DEFPARAM IDENTIFIER BIT    '.' INIT '=' HEX_NUMBER ';'
+                      | DEFPARAM IDENTIFIER VECTOR '.' INIT '=' HEX_NUMBER ';'
+                      | DEFPARAM IDENTIFIER VECTOR '.' IDENTIFIER '=' NUMBER ';'
+                      | DEFPARAM IDENTIFIER VECTOR '.' IDENTIFIER '=' '"' STRING_CON '"' ';'
     '''
     pass
 def p_primitive_port_list(p):
     '''primitive_port_list : primitive_port_list primitive_port
                            | primitive_port
        primitive_port     : '.' IDENTIFIER '(' IDENTIFIER ')' 
+                          | '.' IDENTIFIER '(' IDENTIFIER BIT ')'
+                          | '.' IDENTIFIER '(' IDENTIFIER VECTOR ')'
+                          | '.' IDENTIFIER '(' joint_signal ')'
     '''
     pass
+def p_joint_signal(p):
+    '''joint_signal       : '{' joint_signal_list '}'
+       joint_signal_list  : joint_signal_list signal_element
+                          | signal_element
+    '''
+def p_signal_element(p):
+    '''signal_element : IDENTIFIER BIT
+                      | IDENTIFIER VECTOR
+                      | IDENTIFIER
+    '''
+    pass
+    
 def p_assign_stm_list(p):
     '''assign_stm_list  : assign_stm_list assign_stm
                         | assign_stm
-        assign_stm      : ASSIGN IDENTIFIER '=' IDENTIFIER ';'
+        assign_stm      : ASSIGN signal_element '=' signal_element ';'
+                        | ASSIGN signal_element '=' BIN_NUMBER ';'
     '''
     pass
     
 def p_error(p):
-    print("Syntax error in input!")
+    if p:
+         print "Syntax error at token %s :%s in line %d " % ( p.type , p.value,p.lexer.lineno)
+         exit(0)
+    else:
+         print("Syntax error at EOF")
 
 # Build the parser
 parser = yacc.yacc()
-
 if __name__=='__main__':
     fname=raw_input("plz enter file name to parse:")
     fobj=open(fname)
     all_lines=fobj.read()
     #print all_lines
-    parser.parse(all_lines,debug=True)
+    t=parser.parse(all_lines)
 #    print result
     fobj.close()
--- parsetab.py
+++ parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.5'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'DCF1E6C69F9106E0D00E0826C8F84A0D'
+_lr_signature = '6E642FE0E4FCEE2441E5B4FC2F72F963'
     
-_lr_action_items = {'WIRE':([4,5,10,12,13,23,27,29,31,35,42,43,44,47,],[-6,11,-14,-5,11,-13,-9,-7,-11,-16,-10,-8,-12,-15,]),'DEFPARAM':([63,],[66,]),')':([33,34,46,55,56,60,64,67,],[45,-4,-3,-23,59,-22,67,-24,]),'(':([9,41,58,],[20,51,62,]),'MODULE':([0,],[2,]),'.':([51,55,56,60,67,68,],[54,-23,54,-22,-24,69,]),'VECTOR':([6,7,8,11,],[15,17,19,22,]),'HEX_NUMBER':([71,],[72,]),'ENDMODULE':([39,40,50,61,],[-26,49,-25,-27,]),'OUTPUT':([1,4,5,12,27,29,31,42,43,44,52,],[6,-6,6,-5,-9,-7,-11,-10,-8,-12,-2,]),'INPUT':([1,4,5,12,27,29,31,42,43,44,52,],[7,-6,7,-5,-9,-7,-11,-10,-8,-12,-2,]),';':([14,16,18,21,28,30,32,36,45,57,59,72,],[27,29,31,35,42,43,44,47,52,61,63,73,]),'IDENTIFIER':([2,6,7,8,10,11,13,15,17,19,20,22,23,24,25,26,33,34,35,37,38,46,47,53,54,62,63,65,66,69,73,],[9,14,16,18,-14,21,26,28,30,32,34,36,-13,-18,26,41,46,-4,-16,-17,48,-3,-15,57,58,64,-19,-20,68,70,-21,]),'=':([48,70,],[53,71,]),'ASSIGN':([24,25,37,39,40,50,61,63,65,73,],[-18,38,-17,-26,38,-25,-27,-19,-20,-21,]),'INOUT':([1,4,5,12,27,29,31,42,43,44,52,],[8,-6,8,-5,-9,-7,-11,-10,-8,-12,-2,]),'$end':([3,49,],[0,-1,]),}
+_lr_action_items = {'NUMBER':([115,],[120,]),'INOUT':([1,4,5,12,27,29,31,43,44,45,56,],[8,-7,8,-6,-10,-8,-12,-11,-9,-13,-3,]),'STRING_CON':([119,],[123,]),'WIRE':([4,5,10,12,13,23,27,29,31,35,43,44,45,48,],[-7,11,-15,-6,11,-14,-10,-8,-12,-17,-11,-9,-13,-16,]),')':([33,34,47,61,62,69,70,71,78,79,86,87,88,89,96,97,98,],[46,-5,-4,-34,68,-33,76,77,86,87,-38,-35,96,97,-37,-36,-39,]),'(':([9,42,54,55,67,],[20,53,63,64,74,]),'.':([53,61,62,63,64,69,70,71,86,87,93,96,97,101,103,],[60,-34,60,60,60,-33,60,60,-38,-35,100,-37,-36,105,107,]),'INIT':([100,105,107,],[104,109,112,]),'ENDMODULE':([24,25,37,40,41,52,72,73,75,81,83,84,92,94,95,111,117,122,124,125,127,],[-19,38,-18,-46,51,-45,-47,-48,-20,-22,-26,-21,-25,-23,-24,-27,-28,-30,-31,-29,-32,]),'BIT':([42,50,79,93,],[54,58,89,103,]),'IDENTIFIER':([2,6,7,8,10,11,13,15,17,19,20,22,23,24,25,26,33,34,35,37,39,47,48,50,57,58,59,60,74,75,80,81,82,83,84,90,91,92,94,95,99,105,111,117,122,124,125,127,],[9,14,16,18,-15,21,26,28,30,32,34,36,-14,-19,26,42,47,-5,-17,-18,50,-4,-16,-44,50,-42,-43,67,79,-20,50,-22,93,-26,-21,50,-41,-25,-23,-24,-40,110,-27,-28,-30,-31,-29,-32,]),'=':([49,50,58,59,93,104,109,110,112,],[57,-44,-42,-43,102,108,114,115,116,]),'ASSIGN':([24,25,37,40,41,52,72,73,75,81,83,84,92,94,95,111,117,122,124,125,127,],[-19,39,-18,-46,39,-45,-47,-48,-20,-22,-26,-21,-25,-23,-24,-27,-28,-30,-31,-29,-32,]),'$end':([3,38,51,],[0,-2,-1,]),'MODULE':([0,],[2,]),'"':([115,123,],[119,126,]),'INPUT':([1,4,5,12,27,29,31,43,44,45,56,],[7,-7,7,-6,-10,-8,-12,-11,-9,-13,-3,]),';':([14,16,18,21,28,30,32,36,46,50,58,59,65,66,68,76,77,106,113,118,120,121,126,],[27,29,31,35,43,44,45,48,56,-44,-42,-43,72,73,75,84,85,111,117,122,124,125,127,]),'BIN_NUMBER':([57,],[66,]),'DEFPARAM':([75,81,83,84,85,92,94,95,111,117,122,124,125,127,],[82,82,-26,82,82,-25,82,82,-27,-28,-30,-31,-29,-32,]),'VECTOR':([6,7,8,11,42,50,79,93,],[15,17,19,22,55,59,88,101,]),'OUTPUT':([1,4,5,12,27,29,31,43,44,45,56,],[6,-7,6,-6,-10,-8,-12,-11,-9,-13,-3,]),'{':([74,],[80,]),'HEX_NUMBER':([102,108,114,116,],[106,113,118,121,]),'}':([50,58,59,90,91,99,],[-44,-42,-43,98,-41,-40,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'signal_decl':([5,13,],[10,23,]),'primitive':([13,25,],[24,37,]),'module_decl':([0,],[1,]),'port_decl':([1,5,],[4,12,]),'primitive_port':([51,56,],[55,60,]),'primitive_list':([13,],[25,]),'port_decl_list':([1,],[5,]),'defparam':([63,],[65,]),'signal_decl_list':([5,],[13,]),'vm_file':([0,],[3,]),'pipo_list':([20,],[33,]),'assign_stm':([25,40,],[39,50,]),'assign_stm_list':([25,],[40,]),'primitive_port_list':([51,],[56,]),}
+_lr_goto_items = {'signal_decl':([5,13,],[10,23,]),'primitive':([13,25,],[24,37,]),'module_decl':([0,],[1,]),'port_decl':([1,5,],[4,12,]),'primitive_port':([53,62,63,64,70,71,],[61,69,61,61,69,69,]),'defparam_stm':([75,81,84,85,94,95,],[83,92,83,83,92,92,]),'primitive_list':([13,],[25,]),'port_decl_list':([1,],[5,]),'defparam_list':([75,84,85,],[81,94,95,]),'signal_decl_list':([5,],[13,]),'joint_signal_list':([80,],[90,]),'vm_file':([0,],[3,]),'pipo_list':([20,],[33,]),'assign_stm':([25,41,],[40,52,]),'joint_signal':([74,],[78,]),'assign_stm_list':([25,],[41,]),'primitive_port_list':([53,63,64,],[62,70,71,]),'signal_element':([39,57,80,90,],[49,65,91,99,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,31 +26,52 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> vm_file","S'",1,None,None,None),
-  ('vm_file -> module_decl port_decl_list signal_decl_list primitive_list assign_stm_list ENDMODULE','vm_file',6,'p_vmfile',u'netlist_parser.py',132),
-  ('module_decl -> MODULE IDENTIFIER ( pipo_list ) ;','module_decl',6,'p_module_decl',u'netlist_parser.py',136),
-  ('pipo_list -> pipo_list IDENTIFIER','pipo_list',2,'p_pipo_list',u'netlist_parser.py',140),
-  ('pipo_list -> IDENTIFIER','pipo_list',1,'p_pipo_list',u'netlist_parser.py',141),
-  ('port_decl_list -> port_decl_list port_decl','port_decl_list',2,'p_port_decl',u'netlist_parser.py',146),
-  ('port_decl_list -> port_decl','port_decl_list',1,'p_port_decl',u'netlist_parser.py',147),
-  ('port_decl -> INPUT IDENTIFIER ;','port_decl',3,'p_port_decl',u'netlist_parser.py',148),
-  ('port_decl -> INPUT VECTOR IDENTIFIER ;','port_decl',4,'p_port_decl',u'netlist_parser.py',149),
-  ('port_decl -> OUTPUT IDENTIFIER ;','port_decl',3,'p_port_decl',u'netlist_parser.py',150),
-  ('port_decl -> OUTPUT VECTOR IDENTIFIER ;','port_decl',4,'p_port_decl',u'netlist_parser.py',151),
-  ('port_decl -> INOUT IDENTIFIER ;','port_decl',3,'p_port_decl',u'netlist_parser.py',152),
-  ('port_decl -> INOUT VECTOR IDENTIFIER ;','port_decl',4,'p_port_decl',u'netlist_parser.py',153),
-  ('signal_decl_list -> signal_decl_list signal_decl','signal_decl_list',2,'p_signal_decl_list',u'netlist_parser.py',157),
-  ('signal_decl_list -> signal_decl','signal_decl_list',1,'p_signal_decl_list',u'netlist_parser.py',158),
-  ('signal_decl -> WIRE VECTOR IDENTIFIER ;','signal_decl',4,'p_signal_decl_list',u'netlist_parser.py',159),
-  ('signal_decl -> WIRE IDENTIFIER ;','signal_decl',3,'p_signal_decl_list',u'netlist_parser.py',160),
-  ('primitive_list -> primitive_list primitive','primitive_list',2,'p_primitive_list',u'netlist_parser.py',164),
-  ('primitive_list -> primitive','primitive_list',1,'p_primitive_list',u'netlist_parser.py',165),
-  ('primitive -> IDENTIFIER IDENTIFIER ( primitive_port_list ) ;','primitive',6,'p_primitive_list',u'netlist_parser.py',166),
-  ('primitive -> IDENTIFIER IDENTIFIER ( primitive_port_list ) ; defparam','primitive',7,'p_primitive_list',u'netlist_parser.py',167),
-  ('defparam -> DEFPARAM IDENTIFIER . IDENTIFIER = HEX_NUMBER ;','defparam',7,'p_primitive_list',u'netlist_parser.py',168),
-  ('primitive_port_list -> primitive_port_list primitive_port','primitive_port_list',2,'p_primitive_port_list',u'netlist_parser.py',172),
-  ('primitive_port_list -> primitive_port','primitive_port_list',1,'p_primitive_port_list',u'netlist_parser.py',173),
-  ('primitive_port -> . IDENTIFIER ( IDENTIFIER )','primitive_port',5,'p_primitive_port_list',u'netlist_parser.py',174),
-  ('assign_stm_list -> assign_stm_list assign_stm','assign_stm_list',2,'p_assign_stm_list',u'netlist_parser.py',178),
-  ('assign_stm_list -> assign_stm','assign_stm_list',1,'p_assign_stm_list',u'netlist_parser.py',179),
-  ('assign_stm -> ASSIGN IDENTIFIER = IDENTIFIER ;','assign_stm',5,'p_assign_stm_list',u'netlist_parser.py',180),
+  ('vm_file -> module_decl port_decl_list signal_decl_list primitive_list assign_stm_list ENDMODULE','vm_file',6,'p_vmfile',u'netlist_parser.py',156),
+  ('vm_file -> module_decl port_decl_list signal_decl_list primitive_list ENDMODULE','vm_file',5,'p_vmfile',u'netlist_parser.py',157),
+  ('module_decl -> MODULE IDENTIFIER ( pipo_list ) ;','module_decl',6,'p_module_decl',u'netlist_parser.py',161),
+  ('pipo_list -> pipo_list IDENTIFIER','pipo_list',2,'p_pipo_list',u'netlist_parser.py',165),
+  ('pipo_list -> IDENTIFIER','pipo_list',1,'p_pipo_list',u'netlist_parser.py',166),
+  ('port_decl_list -> port_decl_list port_decl','port_decl_list',2,'p_port_decl',u'netlist_parser.py',170),
+  ('port_decl_list -> port_decl','port_decl_list',1,'p_port_decl',u'netlist_parser.py',171),
+  ('port_decl -> INPUT IDENTIFIER ;','port_decl',3,'p_port_decl',u'netlist_parser.py',172),
+  ('port_decl -> INPUT VECTOR IDENTIFIER ;','port_decl',4,'p_port_decl',u'netlist_parser.py',173),
+  ('port_decl -> OUTPUT IDENTIFIER ;','port_decl',3,'p_port_decl',u'netlist_parser.py',174),
+  ('port_decl -> OUTPUT VECTOR IDENTIFIER ;','port_decl',4,'p_port_decl',u'netlist_parser.py',175),
+  ('port_decl -> INOUT IDENTIFIER ;','port_decl',3,'p_port_decl',u'netlist_parser.py',176),
+  ('port_decl -> INOUT VECTOR IDENTIFIER ;','port_decl',4,'p_port_decl',u'netlist_parser.py',177),
+  ('signal_decl_list -> signal_decl_list signal_decl','signal_decl_list',2,'p_signal_decl_list',u'netlist_parser.py',181),
+  ('signal_decl_list -> signal_decl','signal_decl_list',1,'p_signal_decl_list',u'netlist_parser.py',182),
+  ('signal_decl -> WIRE VECTOR IDENTIFIER ;','signal_decl',4,'p_signal_decl_list',u'netlist_parser.py',183),
+  ('signal_decl -> WIRE IDENTIFIER ;','signal_decl',3,'p_signal_decl_list',u'netlist_parser.py',184),
+  ('primitive_list -> primitive_list primitive','primitive_list',2,'p_primitive_list',u'netlist_parser.py',188),
+  ('primitive_list -> primitive','primitive_list',1,'p_primitive_list',u'netlist_parser.py',189),
+  ('primitive -> IDENTIFIER IDENTIFIER ( primitive_port_list ) ;','primitive',6,'p_primitive_list',u'netlist_parser.py',190),
+  ('primitive -> IDENTIFIER IDENTIFIER BIT ( primitive_port_list ) ;','primitive',7,'p_primitive_list',u'netlist_parser.py',191),
+  ('primitive -> IDENTIFIER IDENTIFIER ( primitive_port_list ) ; defparam_list','primitive',7,'p_primitive_list',u'netlist_parser.py',192),
+  ('primitive -> IDENTIFIER IDENTIFIER BIT ( primitive_port_list ) ; defparam_list','primitive',8,'p_primitive_list',u'netlist_parser.py',193),
+  ('primitive -> IDENTIFIER IDENTIFIER VECTOR ( primitive_port_list ) ; defparam_list','primitive',8,'p_primitive_list',u'netlist_parser.py',194),
+  ('defparam_list -> defparam_list defparam_stm','defparam_list',2,'p_primitive_list',u'netlist_parser.py',195),
+  ('defparam_list -> defparam_stm','defparam_list',1,'p_primitive_list',u'netlist_parser.py',196),
+  ('defparam_stm -> DEFPARAM IDENTIFIER = HEX_NUMBER ;','defparam_stm',5,'p_primitive_list',u'netlist_parser.py',197),
+  ('defparam_stm -> DEFPARAM IDENTIFIER . INIT = HEX_NUMBER ;','defparam_stm',7,'p_primitive_list',u'netlist_parser.py',198),
+  ('defparam_stm -> DEFPARAM IDENTIFIER BIT . INIT = HEX_NUMBER ;','defparam_stm',8,'p_primitive_list',u'netlist_parser.py',199),
+  ('defparam_stm -> DEFPARAM IDENTIFIER VECTOR . INIT = HEX_NUMBER ;','defparam_stm',8,'p_primitive_list',u'netlist_parser.py',200),
+  ('defparam_stm -> DEFPARAM IDENTIFIER VECTOR . IDENTIFIER = NUMBER ;','defparam_stm',8,'p_primitive_list',u'netlist_parser.py',201),
+  ('defparam_stm -> DEFPARAM IDENTIFIER VECTOR . IDENTIFIER = " STRING_CON " ;','defparam_stm',10,'p_primitive_list',u'netlist_parser.py',202),
+  ('primitive_port_list -> primitive_port_list primitive_port','primitive_port_list',2,'p_primitive_port_list',u'netlist_parser.py',206),
+  ('primitive_port_list -> primitive_port','primitive_port_list',1,'p_primitive_port_list',u'netlist_parser.py',207),
+  ('primitive_port -> . IDENTIFIER ( IDENTIFIER )','primitive_port',5,'p_primitive_port_list',u'netlist_parser.py',208),
+  ('primitive_port -> . IDENTIFIER ( IDENTIFIER BIT )','primitive_port',6,'p_primitive_port_list',u'netlist_parser.py',209),
+  ('primitive_port -> . IDENTIFIER ( IDENTIFIER VECTOR )','primitive_port',6,'p_primitive_port_list',u'netlist_parser.py',210),
+  ('primitive_port -> . IDENTIFIER ( joint_signal )','primitive_port',5,'p_primitive_port_list',u'netlist_parser.py',211),
+  ('joint_signal -> { joint_signal_list }','joint_signal',3,'p_joint_signal',u'netlist_parser.py',215),
+  ('joint_signal_list -> joint_signal_list signal_element','joint_signal_list',2,'p_joint_signal',u'netlist_parser.py',216),
+  ('joint_signal_list -> signal_element','joint_signal_list',1,'p_joint_signal',u'netlist_parser.py',217),
+  ('signal_element -> IDENTIFIER BIT','signal_element',2,'p_signal_element',u'netlist_parser.py',220),
+  ('signal_element -> IDENTIFIER VECTOR','signal_element',2,'p_signal_element',u'netlist_parser.py',221),
+  ('signal_element -> IDENTIFIER','signal_element',1,'p_signal_element',u'netlist_parser.py',222),
+  ('assign_stm_list -> assign_stm_list assign_stm','assign_stm_list',2,'p_assign_stm_list',u'netlist_parser.py',227),
+  ('assign_stm_list -> assign_stm','assign_stm_list',1,'p_assign_stm_list',u'netlist_parser.py',228),
+  ('assign_stm -> ASSIGN signal_element = signal_element ;','assign_stm',5,'p_assign_stm_list',u'netlist_parser.py',229),
+  ('assign_stm -> ASSIGN signal_element = BIN_NUMBER ;','assign_stm',5,'p_assign_stm_list',u'netlist_parser.py',230),
 ]
