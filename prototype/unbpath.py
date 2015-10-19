4fdd744651c3c6e65adaf98f079d3ecd5b6c791a
litaotju <litaotju@live.cn>
2015-10-19 20:04:45 +0800

    bug fixed

--- prototype/unbpath.py
+++ prototype/unbpath.py
@@ -1,89 +1,120 @@
 # -*- coding:utf-8 -*- #
 # from Queue import Queue
-'''对于完全图来说，整个算法所需要的消耗的时间好像太长了，以及算法所发现的不平衡边，太多了。
-N = 4的完全图中，有36条不平衡路径。
+'''
+对于完全图来说，整个算法所需要的消耗的时间好像太长了，以及算法所发现的不平衡边，太多了。N = 4的完全图中，有36条不平衡路径。
+Changelog: 
 '''
 import networkx as nx
 
 def unbalance_paths(G):
-	'''
-	@param: G, a nx.DiGraph obj
-	@return：{}  upath2, all unbalance unbpath, 
-			 key: multi fanout node A, value: paths from A
-	'''
-	upath = {}
-	upath2 = {}
-	nodes = [node for node in G.nodes() if G.out_degree(node)>1]
-	for s in nodes:
-		upath[s] = find_upath(G, s)
-		upath2[s] = [] 
-		for t in upath[s]:
-			upath2[s] += nx.all_simple_paths(G, s, t)
-	return upath2
-	
+    '''
+    @param: G, a nx.DiGraph obj
+    @return：{}  upath2, all unbalance unbpath, 
+        key: multi fanout node A, value: paths from A
+    '''
+    # BUGY：该程序不能找到所有的环，只能找到所有的不平衡路径。原因是，在搜索的时候，只搜索了所有
+    # 出度大于1的节点，可以进行改进。将第一步没有搜索的点，全部搜索一遍，从而找出图中的环
+    upath = {}
+    upath2 = {}
+    nodes = [node for node in G.nodes() if G.out_degree(node)>1]
+    for s in nodes:
+        upath[s] = find_upath(G, s)
+        print "Snode, Tnodes: %s %s" % ( str(s),str(upath[s]) )
+        upath2[s] = [] 
+        for t in upath[s]:
+            upath2[s] += nx.all_simple_paths(G, s, t)
+    return upath2
+    
 def find_upath(G, s):
-	'''@param: G, an DiGraph obj
-			 : s, a node in G
-	   @return: reconvergent nodes in from s.
-	'''
-	# print "Searching node: %s" % s
-	time = 0
-	depth = {}
-	depth[s] = [0]
-	reconvergent_nodes = []
-	
-	bfs_list = G.succ[s].keys()
-	while(len(bfs_list) != 0 ):
-		time += 1
-		next_list = []
-		for node in bfs_list:
-			if node is s:
-				# print "A loop contained s found"
-				continue
-			if node not in depth.keys():
-				depth[node] = []
-			else:
-				# print "Node:%s depth:%d" % ( str(node), time )
-				if node not in reconvergent_nodes:
-					reconvergent_nodes.append(node)
-			depth[node].append(time)
-			for next_level_node in G.succ[node].keys():
-				if next_level_node  in next_list or next_level_node in depth.keys():
-					if next_level_node in depth.keys():
-						# print "A reverse Edge: %s %s " % (node , next_level_node)
-						continue	
-				next_list.append(next_level_node)
-		bfs_list = next_list 
-	return reconvergent_nodes
+    '''@param: G, an DiGraph obj
+             : s, a node in G
+        @return: reconvergent nodes in from s.
+    '''
+    # 这个函数是tab来缩进的，，，
+    print "Searching node: %s" % s
+    time = 0
+    depth = {}
+    depth[s] = [0]
+    reconvergent_nodes = []
+    
+    bfs_list = G.succ[s].keys()
+    while(len(bfs_list) != 0 ):
+        time += 1
+        next_list = []
+        print "bfs_list: ",
+        print bfs_list
+        # 传递进来的一定是没有经过访问的标定深度的节点
+        # 所以为每一个标定一下深度
+        for node in bfs_list:
+            if node is s:
+                continue
+            assert not depth.has_key(node) ,"Node: %s" % str(node)
+            depth[node] = (time)
+            #print "Node:%s depth:%d" % ( str(node), time )
+                
+        # 找出下一级的节点，（本级没有包含，前级也没有包含的节点）
+        # 如果本级包含或者前级包含了，那么该节点一定是重汇聚节点
+        for node in bfs_list:
+            for next_level_node in G.succ[node].keys():
+                if depth.has_key(next_level_node):
+                    if not (next_level_node in reconvergent_nodes):
+                        reconvergent_nodes.append(next_level_node)
+                    continue
+                elif next_level_node not in next_list:
+                    next_list.append(next_level_node)
+                else:
+                    continue
+        bfs_list = next_list 
+    return reconvergent_nodes
 #----------------------------------------------------------------------------------
 
 def __stat_complete():
-	'''统计节点数目为1-10的完全图中的不平衡路径的个数
-	'''
-	number = []
-	for i in range (1, 11):
-		G = nx.DiGraph(nx.complete_graph(i))
-		print G.number_of_edges() 
-		u = unbalance_paths(G)
-		j = 0
-		for key, val in u.items():
-			j += len(val)
-		number.append(j)
-	return numbers
-	
+    '''统计节点数目为1-10的完全图中的不平衡路径的个数
+    '''
+    number = []
+    for i in range (1, 11):
+        G = nx.DiGraph(nx.complete_graph(i))
+        print G.number_of_edges() 
+        u = unbalance_paths(G)
+        j = 0
+        for key, val in u.items():
+            j += len(val)
+        number.append(j)
+    return numbers
+    
+    
+def __draw_complete(i):
+    '''@param: i, nodes number of complete complete_graph
+     @brief: just draw a graph 
+    '''
+    import matplotlib.pylab as plt
+    G = nx.DiGraph(nx.complete_graph(i))
+    plt.figure("Complete DiGraph %d" % i)
+    nx.draw(G)
+    plt.show()	
+    return None
+    
 if __name__ == "__main__":
-	G = nx.DiGraph()
-	edges = [
+    G = nx.DiGraph()
+    edges = [
 		('a', 'b'),('a', 'c'),('a','d'),('c','d'),
-		('b','c'),('b','d')
-		# ('c', 'e'),('d','e'),
-		# ('e','f'),('a','a'),
+		('b','c'),('b','d'),
+		('c', 'e'),('d','e'),
+		('e','f'),('a','a'),
 	]
-	G.add_edges_from(edges)
-	# r = find_upath(G, 'a')
-	# print r
-
-	# print "Un-balance Paths:"
-	# for key , val in u.items():
-	# 	print "%s %s " % (str(key), str(val))
+    edges2 = [('a','b'), ('b','c'), ('c','a')]
+    edges3 = [('a','b'),('b','a'),('b','c'),('c','a')]
+    G.add_edges_from(edges2)
+    u = unbalance_paths(G)
+    cycles = []
+    print "Un-balance Paths:"
+    for key , val in u.items():
+        for path in val:
+            if path[0] is path[-1]:
+                cycles.append(path)
+            else:
+                print "%s %s " % (str(key), str(path))
+    print "Cycles Are:"
+    for cycle in cycles:
+        print cycle
 
