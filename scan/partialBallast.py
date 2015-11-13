1d570c06ea1595f600861aa996f7f6d75dbca54e
litaotju <litaotju@live.cn>
2015-11-13 22:49:46 +0800

    两种部分扫描算法

---
+++ scan/partialBallast.py
@@ -0,0 +1,148 @@
+﻿# -*- coding: utf-8 -*-
+
+
+import os
+import networkx as nx
+
+# user-defined module
+import netlistx.class_circuit as cc
+from netlistx.file_util    import vm_files
+
+from netlistx.graph.cloudgraph      import CloudRegGraph
+from netlistx.graph.circuitgraph    import get_graph
+
+__all__ = [ "feedbackset", "balance", "ballast" ]
+
+def feedbackset(graph):
+    assert isinstance( graph, nx.DiGraph )
+    fas = []
+    for c in nx.simple_cycles(graph):
+        # self-loop
+        if len(c) == 1: 
+            edge = (c[0], c[0])
+        else:
+            edge = (c[0], c[1])
+        if not edge in fas:
+            fas.append( edge )
+    graph.remove_edges_from( fas )  
+    return fas
+        
+def balance( graph):
+    assert nx.is_directed_acyclic_graph(graph),\
+        "The target graph you want to banlance is not a DAG"
+    r = [] # removed set
+    if __check(graph):
+        return r
+    cs, g1, g2 = __cut(graph) 
+    r = balance(g1) + balance(g2) + cs
+    csl = []
+    for eachEdge in cs:
+        under_check_graph = graph.copy()
+        under_check_graph.remove_edges_from(r)
+        under_check_graph.add_edges_from(csl)
+        under_check_graph.add_edge(eachEdge[0], eachEdge[1])
+        if __check(under_check_graph):
+            csl.append(eachEdge)
+            graph.add_edge(eachEdge[0],eachEdge[1])
+    for eachEdge in csl:
+        r.remove(eachEdge)
+    return r
+  
+def __check( graph ):
+    roots = [node for node in graph.nodes_iter() if graph.in_degree()[node]==0]
+    if len(roots) < 1:
+        return False
+    # 广度优先搜索,访问根节点，访问根节点的下一层节点， 为每一个点定Level
+    # 访问一层节点的下一层节点，直到将所有的节点访问完，跳出。
+    for root in roots:
+        bfs_queue = [root]
+        level = {root:0 }                  #记录每一个点的LEVEL的字典，初始只记录当前root
+        been_levelized = []                #已经被定级的节点
+        current_level = 0                  #当前的层
+        while(bfs_queue):
+		    # 传进来的bfs_queque的层是已知的，
+		    # 记录下它们的所有后继结点，并为他们定层次为n+1,同时放到待访问序列里面
+            current_level +=1
+            next_level_que = []
+            for eachNode in bfs_queue:
+                for eachSucc in graph.successors(eachNode):
+                    if not eachSucc in next_level_que:
+                        next_level_que.append(eachSucc)
+                        if not level.has_key(eachSucc):
+                            level[eachSucc] = current_level
+                        elif level[eachSucc] ==current_level:
+                            continue
+                        else:
+                            return False
+            been_levelized += bfs_queue
+            bfs_queue = next_level_que
+    return True
+#--------------------------------------------------------------------------------
+    
+def __cut(graph):
+    ''' param: 
+            graph:a nx.DiGraph obj
+	    return:
+		    cs : edge cut set of the graph
+		    g1 , g2 : subgraphs induced by cs
+	
+    '''
+    assert isinstance(graph, nx.DiGraph), "graph class: %s " % graph.__class__
+    assert graph.number_of_nodes() > 1,   "Number of nodes: %d" % graph.number_of_nodes()
+    unigraph = nx.Graph( graph )          
+    cs = nx.minimum_edge_cut( unigraph ) 
+    if not cs:
+        raise Exception,"Cut Set of this graph is Empty"
+
+    #CS中的边，可能不存在于原来的有向图中，所以需要将这种边的方向颠倒
+    #将所有real edge,存到RCS中
+    rcs = []
+    original_edges = graph.edges()
+    for eachEdge in cs:
+        if not eachEdge in original_edges:
+            eachEdge = (eachEdge[1], eachEdge[0]) #调换方向
+        rcs.append(eachEdge)
+    graph.remove_edges_from(rcs)
+    glist = []
+    for eachCntComp in nx.weakly_connected_component_subgraphs(graph, copy = False):
+        glist.append(eachCntComp)
+    assert len(glist) == 2
+    return rcs, glist[0], glist[1]
+
+def convert2int( graph):
+    intgraph = nx.DiGraph(name = graph.name + "_intgraph" )
+    map = {}
+    invmap = {}
+    i = 0
+    for node in graph.nodes_iter():
+        map[node] = i
+        invmap[i] = node
+        i += 1
+    oweight = nx.get_edge_attributes(graph, 'weight' )
+    for edge in graph.edges_iter():
+        intedge = ( map[ edge[0] ], map[ edge[1] ] )
+        intgraph.add_edge(intedge[0], intedge[1], weight = oweight[edge] )
+    return intgraph, map, invmap
+
+def ballast( crgraph):
+    intgraph, map, invmap = convert2int( crgraph )
+    fas = feedbackset( intgraph )
+    r = balance( intgraph )
+    scanfds = []
+    for edge in fas + r:
+        realedge = invmap[edge[0]], invmap[edge[1]]
+        scanfds += crgraph.arcs[realedge]
+    return scanfds
+
+def main():
+    inputfile = raw_input("plz enter file name:")
+    g = get_graph( inputfile )
+    g.info()
+    cr = CloudRegGraph( g )
+    cr.info()
+    scanfds = ballast( cr )
+    print "FD number:", len(cr.constfds + cr.fds)
+    print "SCAN_FD number: ", len(scanfds)
+
+if __name__ == "__main__":
+    main()
---
+++ scan/partialOpt.py
@@ -0,0 +1,169 @@
+﻿# -*-coding:utf-8 -*- #
+import re
+import os
+import sys
+import copy
+
+import networkx as nx
+
+from netlistx.file_util import vm_files
+
+from netlistx.prototype.unbpath import unbalance_paths
+from netlistx.graph.cloudgraph import *
+from netlistx.graph.circuitgraph import CircuitGraph
+from netlistx.graph.circuitgraph import get_graph
+
+def upath_cycle(cr):
+    '''@ param: cr, a nx.DiGraph,每一个节点必须有一个独特的name属性与其他节点不同
+       @ return: upaths, cycles ,图中所有的不平衡路径和环
+       @ brief: 将每一个图转化成节点的名字为节点的图，然后找出图中所有的不平衡路径和环
+    '''
+    tmp = {}
+    for node in cr.nodes_iter():
+        assert not tmp.has_key( node.name), \
+            "Node:%s, has the same name with:%s, name:%s" % (node, tmp[node.name], node.name)
+        tmp[node.name] = node
+
+    namegraph = nx.DiGraph()
+    for edge in cr.edges():
+        namegraph.add_edge( edge[0].name, edge[1].name )
+        
+    upaths = unbalance_paths( namegraph)
+    cycles = []
+    for cycle in nx.simple_cycles(namegraph):
+        cycles.append( cycle)
+    return upaths, cycles
+
+def convert2opt(cr, upaths, cycles):
+    '''@param: cr, a graph, every node is a nx.DiGraph obj
+               eweight, a dict, cr's edge -> weight
+               upaths,  a dict, (s, t )   -> [ all unbalance paths bwt (s, t)]
+               cycles,  a list, [ simple cycle of cr ]
+        转化成Matlab求解的方法,并且打印要求解的问题的matlab程序
+    '''
+    ewight = cr.arcs 
+
+    names ={ node:node.name for node in cr.nodes() }
+    namewight = {}
+    for edge, weight in ewight.iteritems():
+        name0 = names[ edge[0] ]
+        name1 = names[ edge[1] ]
+        assert not namewight.has_key( (name0, name1) )
+        namewight[(name0, name1)] = len(weight)
+
+    edge2x = {}       #名称字典,键为名称边，值为X变量的索引
+    all_edges = {}    #权重字典,键为名称边，值为权重
+    cycle_const = []
+    
+    # 下面进行cycles列表的获取
+    for cycle in cycles:
+        edges = []
+        for i in range(0, len(cycle)-1 ):
+            edge = (cycle[i], cycle[i+1]) 
+            edges.append( edge )
+            all_edges[ edge ] = namewight[ edge ]
+        back_edge = (cycle[-1], cycle[0])
+        all_edges[ back_edge ] = namewight[ back_edge ]
+        edges.append( back_edge )
+        cycle_const.append( edges )
+
+    # 下面字典的每一个Key是（s,t）tuple，值是s.t之间的所有路径的列表
+    unbalance_const = {}
+    for (s,t), path_bwt in upaths.iteritems():
+        if s is t or s == t:
+            continue
+        unbalance_const[ (s,t) ] = []
+        for upath in path_bwt:
+            edges = []
+            for i in range(0, len(upath)-1):
+                edge = (upath[i], upath[i+1])
+                edges.append( edge )
+                all_edges[ edge ] = namewight[ edge ]
+            unbalance_const[ (s,t) ].append( edges )
+
+    # 准备进行权重和约束的输出
+    cnt = 0
+    sorted_wight = []
+    for edge, wight in all_edges.iteritems():
+        cnt += 1 
+        edge2x[ edge ] = "x(%d)" %cnt
+        print "%% x%d: %s -> %s" % (cnt, edge[0], edge[1])
+        sorted_wight.append( wight)
+    print "x = binvar(1, %d);" % len(all_edges)
+
+    SDPSETTING = '''
+    ops = sdpsettings('solver','bnb','bnb.solver','fmincon','bnb.method',...
+                  'breadth','bnb.gaptol',1e-8,'verbose',1,'bnb.maxiter',1000,'allownonconvex',0);
+    '''
+    print SDPSETTING
+    #权重的字符串形式
+    print "%% Weight of Edges"
+    print "W = ", str(sorted_wight ) , ";"
+    
+    print "obj = -x*W';"
+    print "constraints = [..."
+    #环约束的字符串形式
+    cycle_string_const = [] 
+    for cycle in cycle_const:
+        xs = []
+        for edge in cycle:
+            xs.append( edge2x[edge])
+        string = "*".join( xs )
+        string = string+ "<= 1/100000 ;..."
+        cycle_string_const.append( string )
+    print "%% Cycle Constraits Are:"
+    print "\n".join( cycle_string_const)
+
+    #不平衡路径约束的字符串形式
+    print "%% Unbalance Constraints Are:"
+    unbalance_string_const = [] 
+    for (s, t), paths_between in unbalance_const.iteritems():
+        length_dict = {}
+        # 把路径按照长度来归类.同一个长度的不平衡路径全部乘起来，称之为Ki
+        for upath in paths_between:
+            n = len( upath ) 
+            xs = []
+            for edge in upath:
+                xs.append( edge2x[edge] )
+            string = "*".join( xs)
+            if not length_dict.has_key( n ):
+                length_dict[n] = string
+            else:
+                length_dict[n] = length_dict[n]+"*"+string
+        
+        length_list = length_dict.values()
+        print "%% (%s, %s)" % (s,t)
+        print "\n".join([ "%% k%d: %s" %(i+1, length_list[i] ) \
+                            for i in range(0, len(length_list) )] )
+        n = len( length_list )
+        assert n >= 2
+        #将不同的Ki两两相乘
+        for i in range(0, n-1):
+            for j in range(i+1, n):
+                print "%% k%d * k%d" %(i, j)
+                print length_list[i]+"*"+length_list[j] + "<= 1/100000;..." 
+    print "];"
+    SOLVE   = '''solvesdp(constraints,obj, ops);'''
+    DISPLAY = '''for i = 1:%d\n fprintf('x %%d  ',i);\n display(x(i));\n end''' % len(all_edges )
+    ALL_FD = sum([ len(fdlist) for fdlist in ewight.itervalues()])
+    RUSULT = "display( sum(W)-x*W' );"
+    print "%%All fd number is: %d"  % ALL_FD 
+    print SOLVE
+    print DISPLAY
+    print "%%the number to scan:"
+    print RUSULT
+    return None
+
+if __name__ == "__main__":
+    path = raw_input("plz enter path>")
+    for eachvm in vm_files( path ):
+        g = get_graph( os.path.join(path, eachvm) )
+        cr = CloudRegGraph( g )
+        upaths, cycles = upath_cycle( cr)
+    
+        console = sys.stdout 
+        mscript =  open("test\\partialOptMatlab\\" + g.name +".m" ,'w')
+        sys.stdout = mscript
+        convert2opt(cr, upaths, cycles)
+        sys.stdout = console
+        mscript.close()
