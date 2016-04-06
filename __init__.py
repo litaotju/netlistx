0c4f2df44c1665c77e3e70f6bd92de3703da4207
litaotju <litaotju@live.cn>
2016-04-06 14:04:16 +0800

    delete useless comments

--- __init__.py
+++ __init__.py
@@ -3,36 +3,12 @@
 __author__ = ["litao"]
 __verson__ = "0.0.1"
 
-#__all__ = [
-#        # package
-#        "netlist_parser" , 
-        
-#        # modules
-#        "ballast",
-#        "circuitgraph",
-#        "class_circuit",
-#        "crgraph",
-#        "exception",
-#        "file_util",
-#        "graph_top",
-#        "graph_util",
-#        "netlist",
-#        "netlist_util",
-#        "scan_chain_full_replace",
-#        "scan_chain_insert",
-#        "sgraph",
-#        "testbench_generate"
-#    ]
 
-# 所有的 Exception都直接导入到包的顶层
 import exception
 from exception import *
 
-# 进行网表的解析有两种方法。 1.导入parser模块 2.直接导入parser模块的vm_parse方法
-#import netlistx.parser.netlist_parser as parser
 from netlistx.parser.netlist_parser import vm_parse
 
-# 所有与网表相关的类都直接被导入到包的顶层
 import circuit
 from circuit import *
 
