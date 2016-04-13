447b12fc8c70530ad800c8884d2c791344518609
litaotju <litaotju@live.cn>
2016-04-14 00:13:14 +0800

    引入CliApp类,对 graph\main进行重构
    其他子包也可以使用cliapp模块提供的命令行帮助的类

---
+++ cliapp.py
@@ -0,0 +1,145 @@
+# -*-coding: utf-8 -*- 
+import os
+import sys
+
+__all__ = ["CliApp"]
+
+class CliApp:
+    '''基本的命令行程序
+    '''
+    #常量
+    QUIT_COMMANDS = ('quit', 'exit', 'q')
+    MODE_INTERACTIVE = 'interactive'
+    MODE_BATCH = 'batch'
+    DEFAULT_PATH = os.getcwd()
+    DEFAULT_MODE = MODE_INTERACTIVE
+
+    def __init__(self, name = None):
+        self.name = "CliApp" if name == None else name
+        self.call = {}  # cmd:func
+
+        self.mode = CliApp.DEFAULT_MODE # batch or interactive
+        self.path = CliApp.DEFAULT_PATH #默认路径为 os.getcwd()当前路径, 不变式：self.path一直是存在的路径
+        self.current_file = ""  #当前都进来的文件。空字符串或者文件的绝对路径
+        self.cmd  = ""          #当前的命令
+        self.opt  = []          #当前的命令的选项
+        self.next_call_func = self.printUsage  #下一个要调用的函数。默认打印帮助
+        self.history = []       #输入过的命令历史
+        
+        self.addFunction('cd', self.setPath   )
+        self.addFunction('pwd', self.printPath)
+        self.addFunction("ls", self.listPath  )
+        self.addFunction("help", self.printUsage)
+        self.addFunction('history', self.showHistory )
+        self.addFunction('setmode', self.setMode )
+        self.addFunction("readvm", self.getSingleFile)
+        self.addFunction("mode", lambda: sys.stdout.write(self.mode + os.linesep ) )
+        self.addFunction("file", lambda: sys.stdout.write(self.current_file + os.linesep) )
+        if os.name == 'nt': 
+            self.addFunction('cls', lambda: os.system("cls") )
+
+    def setMode(self):
+        u'''设置运行的模式，批处理或者单个文件的模式, i or b
+        '''
+        input_mode = raw_input("mode ( i for interactive and b for batch) > i/b?:") if not self.opt else self.opt[0]
+        if input_mode in ['i','b']:
+            self.mode = CliApp.MODE_BATCH if input_mode =='b' else CliApp.MODE_INTERACTIVE
+        else:
+            print "Warning, did not change the mode correctly. current mode is:", self.mode
+
+    def getCmd( self ):
+        u'''打印netlistx:>prompt提示，并获取命令和选项
+        '''
+        leaf_path = os.path.split( os.path.abspath(self.path ) )[-1] #当前路径的最后一级
+        prompt =  "[%s %s @ %s]>:"% (self.name, self.mode, leaf_path)
+        args = raw_input(prompt).split()
+        cmd = args[0].strip()
+        #根据命令。获取下一个要运行的函数
+        if cmd in self.call.keys():
+            self.cmd = cmd
+            self.next_call_func = self.call[self.cmd]
+        elif cmd in CliApp.QUIT_COMMANDS:
+            print "ByeBye"
+            sys.exit(0)
+        else:
+            print "Cannot found commond:", args
+            self.next_call_func = self.printUsage
+        #如果有额外的参数，则设置额外的参数
+        if len(args) > 1:
+            self.opt = [ opt.strip() for opt in args[1:] ]
+        else:
+            self.opt = []
+        self.history.append( str(args)[1:-1] )
+        
+    def setPath(self):
+        u'''设置工作路径, 如果指定目录不存在，当前目录保持不变，并打印当前绝对目录,用法: cd path
+         '''
+        path = raw_input("plz enter path>") if not self.opt else self.opt[0]
+        #保持path的绝对路径属性
+        if not os.path.isabs(path): 
+            path = os.path.join( self.path, path)
+        #存在则更新，不存在打印当前路径
+        if os.path.exists( path ):
+            self.path = path
+        else:
+            print "Warning: invalid path."
+            self.printPath()
+        return
+        
+    def printPath(self):
+        u'''显示当前绝对路径
+        '''
+        print "Current abs path is: ", self.path
+
+    def listPath(self):
+        u'''列出当前工作目录下的信息
+        '''
+        print "    ".join( os.listdir(self.path) )
+
+    def showHistory(self):
+        u'''列出命令历史
+        '''
+        print os.linesep.join( self.history)
+        
+    def printUsage(self):
+        u'''打印用法信息
+        '''
+        print "--------------------------usage----------------------------"
+        print os.linesep.join( [ "%10.10s : %s" % (cmd, func.__doc__) 
+                                  for cmd, func in self.call.iteritems() ] )
+        print "--------------------------usage----------------------------"
+
+    def getSingleFile(self):
+        u'''预读取一个文件，设置为当前文件。并返回当前的文件名
+        '''
+        filename = raw_input("plz enter a filename:")  if not self.opt else self.opt[0]
+        if not os.path.isabs(filename):
+            filename = os.path.join(self.path, filename)
+        if not os.path.exists(filename) or not os.path.isfile(filename):
+            print "Error: cannot found file: ", filename
+            print "current file is: ", self.current_file
+        else:
+            self.current_file = filename
+        return
+                
+    def addFunction(self, cmd, func):
+        u'''@brief:为app添加新的 cmd-func对，添加新的功能
+            @params:
+                cmd, a string indicate the cmd from user
+                func, a callable object for cmd
+        '''
+        assert callable(func), "%s is not call able" % func
+        if self.call.has_key(cmd):
+            override = raw_input( "Waring: all ready have a cmd%s, do you want to override: (yes?no)" % cmd)
+            if override == 'yes':
+                self.call[cmd] = func
+        else:
+            self.call[cmd] = func
+
+    def run(self):
+        while True:
+            self.getCmd()
+            self.next_call_func()
+
+if __name__ == "__main__":
+    CliApp().run()
--- graph/main.py
+++ graph/main.py
@@ -6,32 +6,24 @@ import logging
 import networkx as nx
 
 import netlistx.graph.crgraph as old
+from netlistx.cliapp import CliApp
 from netlistx.log import logger
-from netlistx.file_util import vm_files2, StdOutRedirect
+from netlistx.file_util import vm_files2
 from netlistx.exception import *
 from netlistx.graph.circuitgraph import get_graph, save_graphs, fanout_stat
 from netlistx.graph.cloudgraph import CloudRegGraph
 from netlistx.graph.esgraph import ESGraph
 
+class GraphMainApp(CliApp):
 
-class GraphFunc:
-    '''一个抽象类，一些命令和函数的绑定，给出命令就运行相应的函数
-    '''
     def __init__(self):
-        self.path = ""
-        self.opath = "" #设置输出路径
-        self.call = {}
-        self.__bindcmd2func()
-
-    def __bindcmd2func(self):
-        u'''将cmd命令和调用的函数绑定
-        '''
-        self.call["help"]  = self.print_usage
-        self.call["cloud"] = self.cloudgraph
-        self.call["graph"] = self.circuitgraph
-        self.call['oldcr'] = self.crgraph 
-        self.call["esgrh"] = self.esgraph
-
+        CliApp.__init__(self, "GraphApp")
+        self.opath = ""
+        self.addFunction("cloud", self.cloudgraph )
+        self.addFunction("graph", self.circuitgraph)
+        self.addFunction('oldcr', self.crgraph )
+        self.addFunction("esgrh", self.esgraph )
+        
     def __setOpath( self, outpath ):
         u'''设置当前的输出路径
         '''
@@ -42,166 +34,79 @@ class GraphFunc:
         if not os.path.exists( self.opath ):
             os.mkdir( self.opath )
         return self.opath
+    
+    def runProcess(self, process):
+        u'''@breif:根据模式，对一个目录或者一个单独的文件运行一个 process, process
+            @param:
+                process, a function( single file) with to handle a file
+            @return:
+                void
+        '''
+        if self.mode == GraphMainApp.MODE_INTERACTIVE:
+            if self.current_file != "":
+                process( self.current_file )
+            else:
+                print "Warning: no current file opened. use command: readvm filename"
+        else:
+            map(process, vm_files2(self.path) )
 
     def crgraph(self):
         u'''为当前目录下的所有vm文件，生成旧的CloudRegGraph, 保存snapshot到 oldCloudGraphs
         '''
         self.__setOpath("oldCloudGraphs")
-        for eachVm in vm_files2(self.path):
+        def __process(eachVm):
             g = get_graph( eachVm )
             g.info()
             cr = old.CloudRegGraph(g, debug = True) 
             cr.info()
             cr.to_gexf_file( self.opath + "\\%s_CloudRegGraph.gexf" % cr.name)
             cr.snapshot( os.path.join( self.opath, g.name) )
+            return
+        self.runProcess( __process )
 
     def cloudgraph(self):
         u'''为当前目录下的所有vm文件 生成CloudRegGraph, 保存snapshot到CloudGraphs
         '''
         self.__setOpath("CloudGraphs")
-        for eachVm in vm_files2(self.path):
+        def __process(eachVm):
             g = get_graph( eachVm )
             g.info()
             cr = CloudRegGraph(g)
             cr.info()
             cr.snapshot( os.path.join( self.opath, g.name ) )
+            return
+        self.runProcess( __process )
 
     def circuitgraph(self):
         u'''和直接生成 图 相关的子程序
         '''
-        usage = os.linesep.join([ u"命令行帮助，可选命令如下",
-                                u"batch:   对当前路径下的vm文件分别生成两个图（包含和不包含PIPO），保存图的信息到 self.opath下",
-                                u"single:  输入一个文件名称，统计其中组合逻辑和FD节点的扇出数目统计",
-                                u"fanout:     输入一个文件，获取并返回图. 打印图的基本信息"])
         self.__setOpath("Graphs")
-        graph = None
-        while True:
-            cmd = raw_input("graph>:")
-            if cmd == "batch" : 
-                for eachfile in vm_files2(self.path):
-                    save_graphs( eachfile, self.opath )
-            elif cmd == "single":
-                fname = os.path.join(self.path, raw_input("enter a filename:") )
-                graph= get_graph( fname )
-                graph.info()
-            elif cmd == "fanout":
-                if graph is not None:
-                    fanout_stat(graph)
-                else:
-                    print "Has no graph in, type: single command to produce one"
-            elif cmd in ["quit", "exit", 'q']:
-                break
-            else:
-                print usage
+        def __process(eachVm):
+            graph= get_graph( eachVm )
+            graph.info()
+            if self.opt and self.opt[0] =='-fanout':
+                fanout_stat(graph)
+            return
+        if self.mode == GraphMainApp.MODE_BATCH:
+            map(save_graphs, vm_files2(self.path), [self.opath]*len( list(vm_files2(self.path )) ) )
+        else:
+            self.runProcess( __process )
         return
 
     def esgraph( self):
         u'''提取当前路径下的所有vm文件的 ESGraph
         '''
         self.__setOpath("ESGraph")
-        for eachVm in vm_files2(self.path):
+        csvfile = os.path.join( self.opath, "records.csv" )
+        def __process(eachVm):
             g = get_graph( eachVm )
-            logger.debug("Get graph OKAY")
             es = ESGraph( g )
-            logger.debug("Get ES OKAY")
+            es.save_info_item2csv( csvfile )
             logger.info( es.info() )
+            return
+        self.runProcess(__process)
+        return 
 
-    def print_usage(self):
-        u'''打印用法信息
-        '''
-        print "--------------------------usage----------------------------"
-        print os.linesep.join( [ "%10.10s : %s" % (cmd, func.__doc__) 
-                                  for cmd, func in self.call.iteritems() ] )
-    def add_function(self, cmd, func):
-        u'''@brief:为app添加新的 cmd-func对，添加新的功能
-            @params:
-                cmd, a string indicate the cmd from user
-                func, a callable object for cmd
-        '''
-        assert callable(func), "%s is not call able" % func
-        if self.call.has_key(cmd):
-            override = raw_input( "Waring: all ready have a cmd%s, do you want to override: (yes?no)" % cmd)
-            if override == 'yes':
-                self.call[cmd] = func
-        else:
-            self.call[cmd] = func
-
-    def run():
-        raise NotImplementedError
-
-
-class GraphMainApp(GraphFunc):
-
-    def __init__(self):
-        GraphFunc.__init__(self)
-        self.DEFAULT_PATH = os.getcwd()
-        self.name = "netlistx"
-
-        self.path = self.DEFAULT_PATH #默认路径为 os.getcwd()当前路径
-        self.cmd  = ""
-        self.opt  = None
-        self.history = []
-        
-        self.add_function('cd', self.setPath   )
-        self.add_function('pwd', self.printPath)
-        self.add_function("ls", self.listPath  )
-        if os.name == 'nt':
-            self.add_function('clear', lambda: os.system("cls") )
-        self.add_function('history', self.showHistory)
-
-    def getCmd( self ):
-        u'''打印netlistx:>prompt提示，并获取命令和选项
-        '''
-        leaf_path = os.path.split( self.path )[-1] #当前路径的最后一级
-        prompt =  "[%s@%s]>:"% (self.name, leaf_path)
-        args = raw_input(prompt).split()
-        self.cmd = args[0]
-        self.opt = args[1:]
-        self.history.append( self.cmd )
-
-    def setPath(self):
-        u'''设置工作路径, 如果指定目录不存在，当前目录保持不变，并打印当前绝对目录
-         '''
-        path = raw_input("plz enter path>") if not self.opt else self.opt[0]
-        #保持path的绝对路径属性
-        if not os.path.isabs(path):
-            path = os.path.join( self.path, path)
-        #存在则更新，不存在打印当前路径
-        if os.path.exists( path ):
-            self.path = path
-        else:
-            print "Warning: invalid path."
-            self.printPath()
-        return
-        
-    def printPath(self):
-        u'''显示当前绝对路径
-        '''
-        print "Current abs path is: ", self.path
-
-    def listPath(self):
-        u'''列出当前工作目录下的信息
-        '''
-        print "    ".join( os.listdir(self.path) )
-
-    def showHistory(self):
-        u'''列出命令历史
-        '''
-        print os.linesep.join( self.history)
-
-    def run(self):
-        while True:
-            self.getCmd()
-            callfunc = self.call['help'] #默认调取help函数
-            try:
-                callfunc = self.call[self.cmd]
-            except KeyError:
-                if self.cmd in [ "exit", "quit", "q"]:
-                    break
-                else:
-                    print "Error: cannot recognize cmd:", self.cmd
-            callfunc()
-    
 if __name__ == "__main__":
     logger.setLevel(logging.INFO)
     app = GraphMainApp()
